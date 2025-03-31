75e44d5708686f3cc1556bf4413d46d5d08e13ef
Monish0210 <monishpatel831@gmail.com>
2025-03-31 11:42:20 +0530

    First Commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        fontSize : {
+            'course-details-heading-small' : ['26px', '36px'],
+            'course-details-heading-large' : ['36px', '44px'],
+            'home-heading-small' : ['28px', '34px'],
+            'home-heading-large' : ['48px', '56px'],
+            'default' : ['15px', '21px'],
+        },
+        gridTemplateColumns : {
+          'auto' : 'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        spacing: {
+           'section-height' : '500px',
+        },
+        maxWidth: {
+          'course-card' : '424px'
+        },
+        boxShadow : {
+          'custom-card' : '0px 4px 15px 2px rgba(0, 0, 0, 0.1)',
+        }
+      },
+    },
+    plugins: [],
+  }
+
+
+//   home heading is used in Hero.jsx of components/student
+
+// gridTemplateColumns is used in CoursesSection.jsx to make the grid of courses responsive for mobile,
+// also used in Testimonials section
+
+// section-height is used in CourseDetails.jsx
+
+// boxShadow and maxWidth are used in CourseDetails.jsx 
